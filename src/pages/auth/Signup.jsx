d28bae092299ec1601daf4c94c7c9fe3b0998292
Raushan <raushankp75@gmail.com>
2023-07-06 22:30:34 +0530

    Signup page all functionality done with Showing error message and show a loader until success also use cloudinary to upload user profile pic to cloudinary

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -3,6 +3,9 @@ import { Box, Button, CardMedia, Grid, Paper, TextField, Typography } from '@mui
 import loginSignupBg from '../../assets/login-signup-bg.jpg'
 import { Link } from 'react-router-dom'
 import profileUpload from '../../assets/profile-upload.png'
+import ErrorMessage from '../../components/ErrorMessage'
+import axios from 'axios'
+import Loader from '../../components/Loader'
 
 const Signup = () => {
 
@@ -14,12 +17,96 @@ const Signup = () => {
     confirmPassword: ''
   });
 
-  const [image, setImage] = useState("");
+  // for profile pic image
+  const [pic, setPic] = useState("https://www.pngmart.com/files/22/User-Avatar-Profile-Download-PNG-Isolated-Image.png");
+
+  // for profile pic success or error message
+  const [picMessage, setPicMessage] = useState(null)
+
+  // for success message
+  const [message, setMessage] = useState(null);
+
+  // for error
+  const [error, setError] = useState(false);
+
+  // for loading
+  const [loading, setLoading] = useState(false);
+
 
 
   // input field change function
   const handleValueChange = (e) => {
     setSignupData({ ...signupData, [e.target.name]: e.target.value })
+  }
+
+
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    console.log(signupData)
+    console.log(pic)
+
+    if (signupData.password !== signupData.confirmPassword) {
+      setMessage('Password and Confirm password do not match')
+    } else {
+      setError(null)
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+
+        setLoading(true)
+        const { data } = await axios.post("http://localhost:8000/api/users", signupData, {
+          config
+        });
+        // setMessage('Signup successfully!')
+        console.log(data)
+        setLoading(false)
+      } catch (error) {
+        setError(error.response.data.message)
+        setLoading(false)
+      }
+
+    }
+  }
+
+
+
+  // posting pic to cloudinary
+  const postDetails = (image) => {
+    // setPicMessage(null)
+
+    if (!image) {
+      return setPicMessage("Please select an image");
+    }
+    setPicMessage(null);
+
+    if (image.type === 'image/jpeg' || image.type === 'image/png') {
+      const data = new FormData();
+      data.append('file', image)
+      data.append('upload_preset', 'notecache')
+      data.append('cloud_name', 'raushancloud')
+
+      fetch('https://api.cloudinary.com/v1_1/raushancloud/image/upload',
+        {
+          method: 'post',
+          body: data
+        }
+      ).then(res => {
+        return res.json()
+      })
+        .then(data => {
+          // console.log(data.url)
+          // setImageUrl(data.url)
+          console.log(data)
+          setPic(data.url.toString());
+        })
+        .catch(err => console.log(err))
+    }else{
+      return setPicMessage('Please select an image');
+    }
   }
 
 
@@ -39,6 +126,10 @@ const Signup = () => {
     <div style={{ background: `url(${loginSignupBg})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 82vh', height: '82vh', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
       <Paper elevation={4} sx={{ width: { xs: '80%', md: '40%' }, padding: { xs: '5% 5%', md: '20px 70px' }, margin: { xs: '0px 5%', md: '0' } }}>
         <Typography sx={{ fontSize: '25px' }}>Signup</Typography>
+
+        {error && <ErrorMessage severity="error">{error}</ErrorMessage>}
+        {message && <ErrorMessage severity="error">{message}</ErrorMessage>}
+        {loading && <Loader />}
         <form>
           <TextField variant='standard' type='name' name='name' value={signupData.name} onChange={handleValueChange} sx={{ margin: '10px 0', borderBottom: '2px solid blue' }} label='Name' placeholder='Enter name' fullWidth required />
           <TextField variant='standard' type='email' name='email' value={signupData.email} onChange={handleValueChange} sx={{ margin: '10px 0', borderBottom: '2px solid blue' }} label='Email' placeholder='Enter email' fullWidth required />
@@ -46,6 +137,9 @@ const Signup = () => {
           <TextField variant='standard' type='password' name='confirmPassword' value={signupData.confirmPassword} onChange={handleValueChange} sx={{ margin: '10px 0', borderBottom: '2px solid blue' }} label='Confirm Password' placeholder='Enter confirm password' fullWidth required />
 
           {/* For select and upload and show image */}
+          {picMessage && (
+            <ErrorMessage severity="error">{picMessage}</ErrorMessage>
+          )}
           <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px' }}>
             <Button
               // variant="contained"
@@ -66,7 +160,7 @@ const Signup = () => {
                 accept='image/*'
                 onChange={(event) => {
                   loadfile(event);
-                  setImage(event.target.files[0])
+                  postDetails(event.target.files[0])
                 }}
               />
             </Button>
@@ -77,7 +171,7 @@ const Signup = () => {
           </Box>
 
 
-          <Button type='submit' color='primary' variant="contained" sx={{ margin: '10px 0' }} fullWidth>Signup</Button>
+          <Button type='submit' onClick={handleSignup} color='primary' variant="contained" sx={{ margin: '10px 0' }} fullWidth>Signup</Button>
 
           {/* <Typography sx={{ textAlign: 'center', fontSize: '13px', margin: '5px 0' }}>OR</Typography> */}
 
